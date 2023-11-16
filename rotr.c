537830bb59a5ca2a238d5065cd40aa00ceee3ea2
majidied <majidimajidi2003@gmail.com>
2023-11-16 08:07:38 -0800

    up work

--- rotr.c
+++ rotr.c
@@ -12,10 +12,13 @@ void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
 
 	copy = *head;
 	if (*head == NULL || (*head)->next == NULL)
+	{
 		return;
-
+	}
 	while (copy->next)
+	{
 		copy = copy->next;
+	}
 	copy->next = *head;
 	copy->prev->next = NULL;
 	copy->prev = NULL;
